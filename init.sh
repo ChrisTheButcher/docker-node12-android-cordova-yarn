1d80891c4aab419952decdf571a81eb96605e8ee
Chris Wijnia <ccwijnia@gmail.com>
2020-04-29 16:37:09 +0800

    init

--- init.sh
+++ init.sh
@@ -1,7 +1,27 @@
-echo Starting Bash
 source ~/.bashrc
-echo node: && node -v 
-echo nvm: && nvm --version
-echo yarn: && yarn -v
-echo vue: && vue -V
-echo cordova: && cordova -v
+
+VERS=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g' \
+  | tr -d '[[:space:]]')
+
+echo ''
+echo ''
+echo '========================================='
+echo '>>             SOMIA DOCKER            <<'
+echo ">>            Version $VERS            <<"
+echo '========================================='
+echo ''
+echo '   node:       '$(node -v)             
+echo '   nvm:        '$(nvm --version)   
+echo '   yarn:       '$(yarn -v)         
+echo '   vue:        '$(vue -V)          
+echo '   cordova:    '$(cordova -v)      
+echo ''
+echo '========================================='
+echo '>>    Starting Container in Bash...    <<' 
+echo '========================================='
+echo ''
+echo ''
